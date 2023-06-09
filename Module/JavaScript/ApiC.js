39189c0e91c7bb5872b949d326cbb0e940aae61a
[ Dvanmeploph - ( Ferly Afriliyan - Itsmeafriliyan ) ] - <122451961+Itsmeafriliyan@users.noreply.github.com>
2023-06-09 21:39:40 +0700

    Delete Module directory

--- Module/JavaScript/ApiC.js
+++
@@ -1,9 +0,0 @@
-// candaan api untuk ganerate joke dalam bentuk img 
-export default function getJoke(imgSelection) {
-  fetch("https://candaan-api.vercel.app/api/image/random")
-    .then(response => response.json())
-    .then(json => {
-      let image = document.getElementById(imgSelection);
-      image.src = json.data.url;
-  });
-}
