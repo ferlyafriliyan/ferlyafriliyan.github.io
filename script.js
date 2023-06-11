9f42009983b37b2efdd95571ffad66a9b01a02af
[ Dvanmeploph - ( Ferly Afriliyan - Itsmeafriliyan ) ] - <122451961+Livian-xyz@users.noreply.github.com>
2023-06-11 22:38:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// Tambahkan kode JavaScript berikut ke dalam file script.js
-
-// Ambil elemen yang dibutuhkan
 const hamburger = document.querySelector('.hamburger');
 const links = document.querySelector('.links');
 const linksList = document.querySelectorAll('.links li a');
 
-// Tambahkan event listener untuk tombol hamburger
 hamburger.addEventListener('click', function() {
-  // Toggle class 'show' pada elemen links
-  links.classList.toggle('show');
+
+    links.classList.toggle('show');
 });
 
-// Tambahkan event listener untuk setiap link pada daftar menu
 linksList.forEach(function(link) {
   link.addEventListener('click', function() {
-    // Hapus class 'show' dari elemen links
     links.classList.remove('show');
   });
 });
