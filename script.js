c372cbbcf074850a4fa7fe0f13f29f205da334d0
[ Dvanmeploph - ( Ferly Afriliyan - Itsmeafriliyan ) ] - <122451961+FerlyXyn@users.noreply.github.com>
2023-06-12 19:49:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const hamburger = document.querySelector('.hamburger');
-const links = document.querySelector('.links');
-const linksList = document.querySelectorAll('.links li a');
-
-hamburger.addEventListener('click', function() {
+function toggleMenu() {
+  const links = document.querySelector('.links');
   links.classList.toggle('show');
-});
+}
 
-linksList.forEach(function(link) {
-  link.addEventListener('click', function() {
-    links.classList.remove('show');
-  });
-});
+feather.replace();
